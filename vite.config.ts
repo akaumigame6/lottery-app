7056990e942721b95d7745fb33c7520d6a585786
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2026-06-15 14:30:11 +0900

    ✨コマンドに合わせてデプロイ設定の変更

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,28 @@ import { viteSingleFile } from "vite-plugin-singlefile";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "./",
-  plugins: [react(), viteSingleFile(), tailwindcss()],
-  server: {
-    port: 3000,
-    strictPort: false,
-    open: true,
-  },
-  build: {
-    target: "esnext",
-    assetsInlineLimit: 100000000,
-    chunkSizeWarningLimit: 100000000,
-  },
+export default defineConfig(() => {
+  // ① コマンドから渡された環境変数をチェック
+  const isLocalBuild = process.env.BUILD_TARGET === "local";
+  return {
+    // ② リポジトリ名に合わせてbaseを変更する（先頭と末尾の / を忘れないように注意！）
+    base: isLocalBuild ? "./" : "/lottery-app/",
+    plugins: [
+      react(),
+      tailwindcss(),
+      // ③ isLocalBuild が true の時だけ単一ファイル化プラグインをオンにする
+      ...(isLocalBuild ? [viteSingleFile()] : []),
+    ],
+    server: {
+      port: 3000,
+      strictPort: false,
+      open: true,
+    },
+    build: {
+      outDir: isLocalBuild ? "dist-local" : "dist",
+      target: "esnext",
+      assetsInlineLimit: 100000000,
+      chunkSizeWarningLimit: 100000000,
+    },
+  };
 });
